2a8b2955e21397619bc073eb9bff2258f16bbdf6
Newt vo <nhutmvo1997@gmail.com>
2020-10-07 23:39:13 -0400

    Unity2D-Tutorial

---
+++ Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to number wizard, yo");
+        Debug.Log("Pick a number! Don't tell me what it is....");
+        Debug.Log("The higher number you can pick is " + max);
+        Debug.Log("The lower number you can pick is " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = higher, Push Down = Lower, Enter = Correct");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+
+        }
+
+        //Detect when the down arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+
+        }
+
+        //Detect when the Return key is pressed down
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Return key was pressed.");
+        }
+    }
+}
+ 
